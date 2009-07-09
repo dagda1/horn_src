c975c6ed3c73e41ee3fbb5d494554c0b8b60e8d3
dagda1 <dagda1@scotalt.net = dagda1 dagda1@scotalt.net>
2009-07-09 14:27:45 +0000

    CommonAssemblyInfo.cs added

---
+++ src/CommonAssemblyInfo.cs
@@ -0,0 +1,11 @@
+
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyCopyrightAttribute("Released under the Apache 2.0 License")]
+[assembly: AssemblyVersionAttribute("0.0.10")]
+[assembly: AssemblyInformationalVersionAttribute("0.0.10")]
+[assembly: AssemblyCompanyAttribute("scotalt.net")]
+[assembly: AssemblyProductAttribute("horn")]
+		
