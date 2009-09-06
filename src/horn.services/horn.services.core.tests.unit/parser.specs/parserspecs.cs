624e89a67acf11598bd5ebfbceb46320e4cbe25b
dagda1 <dagda1@scotalt.net = dagda1 dagda1@scotalt.net>
2009-09-06 14:09:17 +0000

    Horn.Services projects added.

---
+++ src/horn.services/horn.services.core.tests.unit/parser.specs/parserspecs.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace horn.services.core.tests.unit.parser.specs
+{
+    public class When_traversing_the_package_tree : Specification 
+    {
+        protected override void Because()
+        {            
+        }
+    }
+}
