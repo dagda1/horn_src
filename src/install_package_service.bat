1029b0fbbdba63692df0aac46fd2a8ad9d7d3103
dagda1 <dagda1@scotalt.net = dagda1 dagda1@scotalt.net>
2009-10-18 15:46:02 +0000

    Horn.services branch merged into the trunk

---
+++ src/install_package_service.bat
@@ -0,0 +1,5 @@
+C:\WINDOWS\Microsoft.NET\Framework\v2.0.50727\InstallUtil.exe -u "Horn.PackageBuilder.Service\bin\Debug\Horn.PackageBuilder.Service.exe"
+
+C:\WINDOWS\Microsoft.NET\Framework\v2.0.50727\InstallUtil.exe -i "Horn.PackageBuilder.Service\bin\Debug\Horn.PackageBuilder.Service.exe"
+
+PAUSE
