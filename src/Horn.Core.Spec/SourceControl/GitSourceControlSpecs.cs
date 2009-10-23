32bfb8361d4dea93ead581192b564256c3d9b42e
dagda1 <dagda1@scotalt.net = dagda1 dagda1@scotalt.net>
2009-10-23 10:55:46 +0000

    GitSourceControlSpecs updated

--- src/Horn.Core.Spec/SourceControl/GitSourceControlSpecs.cs
+++ src/Horn.Core.Spec/SourceControl/GitSourceControlSpecs.cs
@@ -1,13 +1,16 @@
+using System;
+using System.Diagnostics;
 using Horn.Core.PackageStructure;
 using Horn.Core.Tree.MetaDataSynchroniser;
 using Horn.Core.Utils;
+using Horn.Framework.helpers;
 using Horn.Spec.Framework.helpers;
 using Horn.Spec.Framework.Stubs;
 using Xunit;
 
 namespace Horn.Core.Spec.SCM
 {
-    public class A_When_the_source_does_not_exist_on_the_client : Specification 
+    public class When_the_source_does_not_exist_on_the_client : Specification 
     {
         private GitSourceControlDouble gitSourceControl;
 
@@ -28,6 +31,32 @@ namespace Horn.Core.Spec.SCM
         [Fact]
         public void Then_the_source_is_retrieved()
         {            
+            Assert.NotEqual(gitSourceControl.Revision, Guid.Empty.ToString());
+        }
+    }
+
+    public class When_the_source_does_exist_on_the_client : Specification
+    {
+        private GitSourceControlDouble gitSourceControl;
+
+        private IPackageTree packageTree;
+
+        protected override void Before_each_spec()
+        {
+            packageTree = TreeHelper.GetTempPackageTree().RetrievePackage(PackageTreeHelper.PackageWithRevision);
+
+            gitSourceControl = new GitSourceControlDouble(MetaDataSynchroniser.PackageTreeUri, new EnvironmentVariable());
+        }
+
+        protected override void Because()
+        {
+            gitSourceControl.RetrieveSource(packageTree);
+        }
+
+        [Fact]
+        public void Then_the_source_is_retrieved()
+        {
+            Assert.False(string.IsNullOrEmpty(gitSourceControl.Revision));
         }
     }
 }
